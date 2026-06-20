a0fc9dce91a23400c1df64ffce0193fbc18751d1
mrromansergeevichgreen <mr.roman.sergeevich.green@gmail.com>
2026-06-21 00:04:53 +0300

    remove fetch_coordinates from restaurateur

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -97,24 +97,6 @@ def view_restaurants(request):
     })
 
 
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
-
-
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     yandex_api_key = settings.YANDEX_API_KEY
